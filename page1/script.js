6d6c090d4920f366ac0a0527ba41e5890092ea24
pramote <manaziza001@gmail.com>
2024-08-31 05:56:33 +0700

    another1

--- page1/script.js
+++
@@ -1,4 +0,0 @@
-function showSection(sectionId) {
-        document.querySelectorAll('.section').forEach(el => el.style.display = 'none');
-        document.getElementById(sectionId).style.display = 'block'
-}
